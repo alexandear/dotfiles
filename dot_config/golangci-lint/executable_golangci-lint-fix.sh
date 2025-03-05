021930cd403af83a78e5f4c790b40fa1c0c9026c
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-03-05 17:07:48 +0200

    .config/golangci-lint: move .golangci.yml, golangci-lint-fix.sh

---
+++ dot_config/golangci-lint/executable_golangci-lint-fix.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Enable command echoing
+set -x
+
+GOLANGCI_LINT=~/src/github.com/golangci/golangci-lint/golangci-lint
+
+AUTOFIX_LINTERS=(
+  "canonicalheader"
+  "dupword"
+  "errorlint"
+  "fatcontext"
+  "gci"
+  "ginkgolinter"
+  "gocritic"
+  "godot"
+  "err113"
+  "gofmt"
+  "gofumpt"
+  "goheader"
+  "goimports"
+  "gosimple"
+  "govet"
+  "iface"
+  "importas"
+  "intrange"
+  "mirror"
+  "misspell"
+  "nakedret"
+  "nlreturn"
+  "perfsprint"
+  "protogetter"
+  "revive"
+  "staticcheck"
+  "stylecheck"
+  "tagalign"
+  "testifylint"
+  "whitespace"
+  "wsl"
+  "nolintlint"
+)
+
+for linter in "${AUTOFIX_LINTERS[@]}"; do
+  echo "--- Fixing $linter issues ---"
+  $GOLANGCI_LINT run --enable-only="$linter" --config=~/.golangci.yml --fix
+done
